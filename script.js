f80584dfdee651edc59a8c4143d1e9a36a9d2ac3
Daniel Kulka <kulkad13@gmail.com>
2021-11-17 10:11:24 -0300

    danielK

--- script.js
+++ script.js
@@ -13,47 +13,76 @@ const frase = "O Carlos trollou e agora terei que pintar o cabelo";
 ////////////
 
 function soma(array) {
-  // Adicione seu código
+  let result = 0;
+    for (let i = 0; i < array.length; i++) {
+      result += array[i];
+    }
+  return result;
 }
 //Retire comentário do código a seguir para testar
-//console.log('Retorno função soma:', soma(numeros));
+// console.log('Retorno função soma:', soma(numeros));
 
 ///////////////////////////////////////////////////////////
 // Teste 2 /
 ////////////
 
 function diff(array) {
-  // Adicione seu código
+  let result = 0;
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] % 2 === 0) {
+        result += array[i] * 2;
+      }
+      else {
+        result += array[i] * 3;
+      }
+    }
+  return result;
 }
 // Retire comentário do código a seguir para testar
-//console.log('Retorno função diff:', diff(numeros));
+// console.log('Retorno função diff:', diff(numeros));
 
 ///////////////////////////////////////////////////////////
 // Teste 3 /
 ////////////
 
 function stringImpar(string) {
-  // Adicione seu código
+  let result = [];
+  let stringSeparada = string.split(" ");
+    for (let i = 0; i < stringSeparada.length; i++) {
+      if (stringSeparada[i].length % 2 !== 0) {
+        result.push(stringSeparada[i]);
+      }
+    }
+  return result;
 }
 //Retire comentário do código a seguir para testar
-//console.log('Retorno função stringImpar:',stringImpar(frase));
+// console.log('Retorno função stringImpar:',stringImpar(frase));
 
 ///////////////////////////////////////////////////////////
 // Teste 4 /
 ////////////
 
 function stringComp(string) {
-  // Adicione seu código
+  let result = [];
+  let stringSeparada = string.split(" ");
+    for (let i = 0; i < stringSeparada.length; i++) {
+      result.push(stringSeparada[i].length);
+    }
+  return result;
 }
 //Retire comentário do código a seguir para testar
-//console.log('Retorno função stringComp:', stringComp(frase));
+// console.log('Retorno função stringComp:', stringComp(frase));
 
 ///////////////////////////////////////////////////////////
 // Teste 5 /
 ////////////
 
 function gnirts(string) {
-  // Adicione seu código
+  let result = " ";
+    for (let i = string.length; i >= 0; i--) {
+      result += string[i - 1];
+    }
+  return result;
 }
 //Retire comentário do código a seguir para testar
-//console.log('Retorno função gnirts:', gnirts(frase));
+console.log('Retorno função gnirts:', gnirts(frase));
